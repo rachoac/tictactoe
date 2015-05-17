3de4ec83bf2d78e6031510705314d3736271b5c3
rachoac <aron.racho@gmail.com>
2015-05-17 16:50:25 -0700

    refatoring to make room for the game service

---
+++ lobby/src/test/java/com/racho/tictactoe/lobby/logic/impl/TestLobbyDAOImpl.java
@@ -0,0 +1,11 @@
+package com.racho.tictactoe.lobby.logic.impl;
+
+import javax.inject.Singleton;
+
+/**
+ * Created by aron on 5/16/15.
+ */
+@Singleton
+public class TestLobbyDAOImpl extends LobbyDAOImpl {
+
+}
