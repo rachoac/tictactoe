677302a6fd3f36b0c72901c6ff0b6643aa6dfbc7
rachoac <aron.racho@gmail.com>
2015-05-16 22:10:27 -0700

    lobby

--- src/main/java/com/racho/tictactoe/lobby/logic/impl/LobbyDAOImpl.java
+++ src/main/java/com/racho/tictactoe/lobby/logic/impl/LobbyDAOImpl.java
@@ -1,50 +1,85 @@
 package com.racho.tictactoe.lobby.logic.impl;
 
+import com.google.common.collect.Lists;
 import com.racho.tictactoe.lobby.logic.Challenge;
 import com.racho.tictactoe.lobby.logic.LobbyDAO;
 import com.racho.tictactoe.lobby.logic.Player;
 
 import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Singleton
+/**
+ * xxx - todo - backed by crude in memory db
+ * xxx - todo - this implementation could be used by the test dao
+ */
 public class LobbyDAOImpl implements LobbyDAO {
+
+    Map<String, Player> joinedPlayers = new HashMap<>();
+    Map<String, Challenge> challenges = new HashMap<>();
+    Map<String, List<String>> playerChallenges = new HashMap<>();
+
     public Player getPlayer(String playerName) {
-        return null;
+        return joinedPlayers.get(playerName);
     }
 
     public void savePlayer(Player player) {
-
+        joinedPlayers.put( player.getPlayerName(), player );
+        playerChallenges.put( player.getPlayerName(), new ArrayList<>() );
     }
 
-    @Override
     public void removePlayer(String playerName) {
-
+        joinedPlayers.remove( playerName );
+        playerChallenges.remove( playerName );
     }
 
-    @Override
     public Challenge getChallenge(String challengeID) {
-        return null;
+        return challenges.get( challengeID );
     }
 
-    @Override
     public void createChallenge(Challenge challenge) {
-
+        challenges.put( challenge.getChallengeID(), challenge );
+        List<String> challengeIDs = playerChallenges.get(challenge.getChallengedPlayer());
+        if ( challengeIDs == null ) {
+            challengeIDs = new ArrayList<>();
+            playerChallenges.put( challenge.getChallengedPlayer(), challengeIDs );
+        }
+        challengeIDs.add( challenge.getChallengeID() );
     }
 
-    @Override
     public void removeChallenge(String challengeID) {
-
+        challenges.remove(challengeID);
+        playerChallenges
+                .keySet()
+                .stream()
+                .filter(s -> s.equals(challengeID));
     }
 
-    @Override
+    // lambda
+    private Function<String, Challenge> idToChallenge = id -> challenges.get(id);
+
     public List<Challenge> getChallengesFor(String challengedPlayer) {
-        return null;
+        List<Challenge> challenges = Lists.newArrayList();
+        List<String> challengeIDs = playerChallenges.get(challengedPlayer);
+        if ( challengeIDs == null ) {
+            return challenges;
+        }
+
+        challenges = challengeIDs.stream()
+                .map(idToChallenge)
+                .collect(Collectors.<Challenge> toList());
+
+        return challenges;
     }
 
     @Override
     public void saveChallenge(Challenge challenge) {
-
+        challenges.put(challenge.getChallengeID(), challenge);
     }
 }
 
--- src/test/java/com/racho/tictactoe/lobby/logic/impl/TestLobbyDAOImpl.java
+++ src/test/java/com/racho/tictactoe/lobby/logic/impl/TestLobbyDAOImpl.java
@@ -17,67 +17,6 @@ import java.util.stream.Collectors;
  * Created by aron on 5/16/15.
  */
 @Singleton
-public class TestLobbyDAOImpl implements LobbyDAO {
+public class TestLobbyDAOImpl extends LobbyDAOImpl {
 
-    Map<String, Player> joinedPlayers = new HashMap<>();
-    Map<String, Challenge> challenges = new HashMap<>();
-    Map<String, List<String>> playerChallenges = new HashMap<>();
-
-    public Player getPlayer(String playerName) {
-        return joinedPlayers.get(playerName);
-    }
-
-    public void savePlayer(Player player) {
-        joinedPlayers.put( player.getPlayerName(), player );
-        playerChallenges.put( player.getPlayerName(), new ArrayList<>() );
-    }
-
-    public void removePlayer(String playerName) {
-        joinedPlayers.remove( playerName );
-        playerChallenges.remove( playerName );
-    }
-
-    public Challenge getChallenge(String challengeID) {
-        return challenges.get( challengeID );
-    }
-
-    public void createChallenge(Challenge challenge) {
-        challenges.put( challenge.getChallengeID(), challenge );
-        List<String> challengeIDs = playerChallenges.get(challenge.getChallengedPlayer());
-        if ( challengeIDs == null ) {
-            challengeIDs = new ArrayList<>();
-            playerChallenges.put( challenge.getChallengedPlayer(), challengeIDs );
-        }
-        challengeIDs.add( challenge.getChallengeID() );
-    }
-
-    public void removeChallenge(String challengeID) {
-        challenges.remove(challengeID);
-        playerChallenges
-            .keySet()
-            .stream()
-            .filter(s -> s.equals(challengeID));
-    }
-
-    // lambda
-    private Function<String, Challenge> idToChallenge = id -> challenges.get(id);
-
-    public List<Challenge> getChallengesFor(String challengedPlayer) {
-        List<Challenge> challenges = Lists.newArrayList();
-        List<String> challengeIDs = playerChallenges.get(challengedPlayer);
-        if ( challengeIDs == null ) {
-            return challenges;
-        }
-
-        challenges = challengeIDs.stream()
-                .map(idToChallenge)
-                .collect(Collectors.<Challenge> toList());
-
-        return challenges;
-    }
-
-    @Override
-    public void saveChallenge(Challenge challenge) {
-        challenges.put(challenge.getChallengeID(), challenge);
-    }
 }
