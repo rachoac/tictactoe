c1a9387e268f4ebf4227e7251d6a115fdcbac80e
rachoac <aron.racho@gmail.com>
2015-05-16 22:06:06 -0700

    adding docs

--- src/main/java/com/racho/tictactoe/lobby/logic/Lobby.java
+++ src/main/java/com/racho/tictactoe/lobby/logic/Lobby.java
@@ -5,23 +5,74 @@ package com.racho.tictactoe.lobby.logic;
  */
 public interface Lobby {
 
+    /**
+     * Confirm whether or not the named player is in the lobby and is ready for a game challenge
+     * @param playerName
+     * @return
+     */
     public boolean isPlayerJoined( String playerName );
 
+    /**
+     * Join a named player to the lobby, indicating that they are ready for a game challenge
+     * @param playerName
+     * @return
+     */
     public Player joinPlayer( String playerName);
 
+    /**
+     * Remove a named player from the lobby. This destroys any pending game challenges
+     * @param playerName
+     */
     public void removePlayer( String playerName );
 
+    /**
+     * Creates a challenge between two players. If the challenge already exists, then an
+     * exception is thrown
+     * @param challengerPlayer
+     * @param challengedPlayer
+     * @return
+     */
     public Challenge createChallenge( String challengerPlayer, String challengedPlayer );
 
+    /**
+     * Returns the first non expired game challenge offered to a player
+     * @param challengedPlayer
+     * @return
+     */
     public Challenge getChallengeFor( String challengedPlayer );
 
+    /**
+     * Removes the named challenge if its expired
+     * @param challengeID
+     * @return
+     */
     public boolean removeExpiredChallenge( String challengeID );
 
+    /**
+     * Confirms that a challenge has been accepted. If it doesn't exist, returns
+     * false.
+     * @param challengeID
+     * @return
+     */
     public boolean isChallengeAccepted( String challengeID );
 
-    boolean acceptChallenge(String challengeID);
+    /**
+     * Indicates that the challenge has been accepted
+     * @param challengeID
+     * @return
+     */
+    public boolean acceptChallenge(String challengeID);
 
-    boolean rejectChallenge(String challengeID);
+    /**
+     * Indicates that the challenge has been rejected
+     * @param challengeID
+     * @return
+     */
+    public boolean rejectChallenge(String challengeID);
 
-    void removeChallenge(String challengeID);
+    /**
+     * Destroys the named challenge
+     * @param challengeID
+     */
+    public void removeChallenge(String challengeID);
 }
