214388ff9b2ada7936f2bd4fd5c9e289894b5722
rachoac <aron.racho@gmail.com>
2015-05-23 16:51:24 -0700

    added dispatcher skeleton

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Flux from 'flux';
+import $ from 'jquery';
+import Events from 'events';
+
+var Dispatcher = new Flux.Dispatcher();
+var Emitter = new Events.EventEmitter();
 
 class Player extends React.Component {
     constructor(props) {
@@ -24,6 +30,13 @@ class PlayerList extends React.Component {
         this.state = {
             players : props.players || []
         }
+    }
+
+    componentWillMount() {
+        Emitter.on("load-player-list", function(players) {
+            debugger;
+            this.setState({ players: players });
+        }.bind(this) );
     }
 
     render() {
@@ -55,6 +68,14 @@ class Lobby extends React.Component {
 
             {
                 'name' : 'sarah'
+            },
+
+            {
+                'name' : 'samuel'
+            },
+
+            {
+                'name' : 'adam'
             }
         ];
 
@@ -65,5 +86,32 @@ class Lobby extends React.Component {
         );
     }
 }
+
+var LobbyStore = function() {
+    debugger;
+    Dispatcher.register( function(payload) {
+        switch (payload.type) {
+            case "load-player-list" : {
+                this._loadPlayerList();
+                break;
+            }
+        }
+
+    }.bind(this) );
+
+    this._loadPlayerList = function() {
+        $.get('http://localhost:9090/lobby/players', function(players) {
+            this._notify(players);
+        }.bind(this));
+    };
+
+    this._notify = function(players) {
+        Emitter.emit("player-list-changed", items);
+
+    }
+
+}
+
+var lobbyStore = new LobbyStore();
 
 React.render(<Lobby/>, document.body);
